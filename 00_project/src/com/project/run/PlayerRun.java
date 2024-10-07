50d872dfad9c3e83348ac092372084b57b67cc9c
manggo999 <manggo999@gmail.com>
2024-10-08 08:59:09 +0900

    토이프로젝트

---
+++ 00_project/src/com/project/run/PlayerRun.java
@@ -0,0 +1,17 @@
+package com.project.run;
+
+import com.project.model.service.PlayerService;
+
+public class PlayerRun {
+
+	public static void main(String[] args) {
+		
+		PlayerService service = new PlayerService();
+		
+		service.displayMenu();
+		
+		
+
+	}
+
+}
